7d8ae16f114ba5686ee0a5a1bd942bb3bf80690f
Jayden Griffith <griffithjayden97@outlook.com>
2022-07-07 17:31:36 -0600

    Added a function file to generate unique IDs
    Added a Router.delete function that has yet to bear fruit.0

---
+++ Develop/helpers/uniqueId.js
@@ -0,0 +1,3 @@
+module.exports = () =>
+  Math.ceil((Math.random()) * 100000)
+  .toString(16)
--- Develop/routes/index.js
+++ Develop/routes/index.js
@@ -1,11 +1,30 @@
 const express = require('express')
 const db = require('express').Router();
 const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const uniqueId = require('../helpers/uniqueId');
+const notes = require("../db/notes.json")
 
 
 db.get('/notes', (req, res) => {
   console.info(`${req.method} request for notes received.`)
   readFromFile('./db/notes.json').then((data) => res.json(JSON.parse(data)))
+})
+
+db.get('/notes/:id', (req, res) => {
+  if (req.params.id) {
+    console.info(`${req.method} request for individual id received.`)
+    const noteId = req.params.id;
+    for (let i = 0; i < notes.length; i++) {
+      const currentNote = notes[i];
+      if (currentNote.id === noteId) {
+        res.status(200).json(currentNote)
+        return
+      }
+    }
+    res.status(404).send('Note not found.')
+  } else {
+    res.status(400).send('Note id not provided.')
+  }
 })
 
 db.post('/notes', (req, res) => {
@@ -18,6 +37,7 @@ db.post('/notes', (req, res) => {
     const newNote = {
       title,
       text,
+      id: uniqueId(), 
     }
 
     readAndAppend(newNote, './db/notes.json')
@@ -34,7 +54,21 @@ db.post('/notes', (req, res) => {
 })
 
 db.delete('/notes/:id', (req, res) => {
-  console.info(`${req.method} request received.`)
+  if (req.params.id) {
+    console.info(`${req.method} request received!`);
+    const noteId = req.params.id;
+    for (let i = 0; i < notes.length; i++) {
+      const currentNote = notes[i];
+      if (currentNote.id === noteId) {
+        notes.splice(currentNote)
+        res.status(200).send('Note Deleted')
+      } 
+    }
+  }
+
+
 })
 
 module.exports = db;
+
+// http://localhost:3001/api/notes/47
